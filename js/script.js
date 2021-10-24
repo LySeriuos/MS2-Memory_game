de35066bc9669c27f50f0a32dbd13f3202b42320
LySeriuos <drungilas.p@gmail.com>
2021-10-24 16:14:59 +0000

    fixed hyperlinks to opeen specific place on the module

--- js/script.js
+++ js/script.js
@@ -9,6 +9,15 @@ $(document).ready(function(){
     function closeModal(){
         $('#modal-container').hide();
     };
+
+    $(document).ready(function () {
+
+        $('.modal').on("hidden.bs.modal", function (e) { //fire on closing modal box
+               if ($('.modal:visible').length) { // check whether parent modal is opend after child modal close
+                   $('body').addClass('modal-open'); // if open mean length is 1 then add a bootstrap css class to body of the page
+               }
+           });
+       });
    
 
     // Game will start and modal will close after pressing button "Start"  
